cd29743331c4d4b417a43b0a5f94ea376898693b
Hamza Ali <plomenet@gmail.com>
2022-08-26 19:57:04 +0800

    Update work.sh

--- work.sh
+++ work.sh
@@ -1,1 +1,1 @@
-/Users/myname/.nvm/versions/node/v16.10.0/bin/node /Users/myname/tasks/task-runner.js --task-path /Users/myname/tasks/tasks.json 2> tasks.log
+/Path/to/task-runner.js --task-path /Path/to/tasks.json 2> tasks.log
